a3543153afdb4736cc9c87091fc084089454f83c
zenitogr <zenitogr@gmail.com>
2025-03-06 11:40:32 +0200

    papas

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import { Bot } from './bot/Bot.js';
-import dotenvx from '@dotenvx/dotenvx';
 import { Logger } from './utils/helpers/Logger.js';
 
-// Try to load .env file but don't fail if it doesn't exist
-try {
-  dotenvx.config({ path: ['.env'], override: false });
-} catch (error) {
-  Logger.info('No .env file found, using system environment variables');
-}
-
-// Add debug logging to help diagnose the issue
-Logger.info('Current environment variables:', {
+// Debug logging
+Logger.info('Environment check:', {
   NODE_ENV: process.env.NODE_ENV,
   DISCORD_BOT_TOKEN: process.env.DISCORD_BOT_TOKEN ? '[PRESENT]' : '[NOT FOUND]'
 });
